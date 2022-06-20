c9351e15e42616045d45fa756c2f098dee26ab4d
wang <www.1185349505@qq.com>
2022-06-20 09:01:25 +0800

    first commit

---
+++ src/main/java/cn/wang/git/GitTest.java
@@ -0,0 +1,16 @@
+package cn.wang.git;
+
+/**
+ * @program: gitDay01
+ * @description:
+ * @Package: cn.wang.git
+ * @ClassName: GitTest
+ * @author: wang
+ * @create: 2022/06/20 08:57
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
